589d4bf4b05999c4b285ae79a2f8d8332086cf72
kowsar <salavemulak@gmail.com>
2021-04-19 15:41:37 +0530

    employee second commit

--- src/constructor/injection/Employee.java
+++ src/constructor/injection/Employee.java
@@ -2,7 +2,8 @@ package constructor.injection;
 
 public class Employee {
 	private int id;  
-	private String name;  
+	private String name; 
+	private Address address;
 	  
 	public Employee() {System.out.println("def cons");}  
 	  
@@ -14,9 +15,16 @@ public class Employee {
 	    this.id = id;  
 	    this.name = name;  
 	}  
+	public Employee(int id, String name, Address address) {  
+	    super();  
+	    this.id = id;  
+	    this.name = name;  
+	    this.address = address;  
+	}
 	  
 	public void show(){  
-	    System.out.println(id+" "+name);  
+	    System.out.println(id+" "+name); 
+	    System.out.println(address.toString()); 
 	}  
 	  
 
